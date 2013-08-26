7aaac89b4865a543e3902fe63ad1aa00b0ce5e5f
Pablo Erartes (INDT) <pabloeuy@gmail.com>
2013-08-26 11:43:14 -0300

    Function isDate() (javascript)

--- motte.js
+++ motte.js
@@ -40,12 +40,50 @@ function mteNotSelect(frm) {
 }
 
 
-function isDate(date){
-	var  ExpRegDate = /^(3[01]|0?[1-9]|[12]d)/(0?[1-9]|1[012])/d{4}/;
+// ******************************************************************
+// This function accepts a string variable and verifies if it is a
+// proper date or not. It validates format matching either
+// mm-dd-yyyy or mm/dd/yyyy. Then it checks to make sure the month
+// has the proper number of days, based on which month it is.
 
-    var res=true;
-    if (!ExpRegDate.test(date) ){
-    	res=false;
+// The function returns true if a valid date, false if not.
+// ******************************************************************
+function isDate(dateStr) {
+	var datePat = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+	var matchArray = dateStr.match(datePat); // is the format ok?
+	var error = '';
+
+	if (matchArray == null) {
+		error = "Please enter date as either mm/dd/yyyy or mm-dd-yyyy.");
+		return false;
 	}
-    return res;
+
+	month = matchArray[1]; // p@rse date into variables
+	day = matchArray[3];
+	year = matchArray[5];
+
+	if (month < 1 || month > 12) { // check month range
+		error = "Month must be between 1 and 12.";
+		return false;
+	}
+
+	if (day < 1 || day > 31) {
+		error = "Day must be between 1 and 31.";
+		return false;
+	}
+
+	if ((month==4 || month==6 || month==9 || month==11) && day==31) {
+		error = "Month "+month+" doesn`t have 31 days!";
+		return false;
+	}
+
+	if (month == 2) { // check for february 29th
+		var isleap = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+		if (day > 29 || (day==29 && !isleap)) {
+			error = "February " + year + " doesn`t have " + day + " days!";
+			return false;
+		}
+	}
+
+	return true; // date is valid
 }
